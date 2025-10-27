313955c5da47f0e1fa785e4fdf476d17df9e1b8f
Minjung Cho <katelyn5897@gmail.com>
2025-10-27 18:30:30 -0400

    ugh wth is going on

--- source/main.jsx
+++ source/main.jsx
@@ -1,12 +1,48 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-window.addEventListener("load", () => {
+import colorSchemes from "./colors.json";
+
+// window.addEventListener("load", () => {
+//   await fetch("https://wwww.colourlovers.com/api/palettes/new?format=json", {
+//     mode: "no-cors"});
+
+//     console.log("response");
+
+//     let colorSchemes = await response.json();
+//     console.log(colorSchemes);
+
+//   let root = createRoot(document.getElementById("root"));
+//   root.render(<App />);
+// });
+
+window.addEventListener("load", async () => {
   let root = createRoot(document.getElementById("root"));
-  root.render(<App />);
+  root.render(<App schemes={colorSchemes} />);
 });
 
-function App() {
+function App({ schemes }) {
   // Your app code goes here...
-  return <p>(This JSX code becomes the contents of your component)</p>;
+  let [sort, updateSort] = useState("id");
+
+  let schemeComponents = [];
+
+  schemes = schemes.toSorted();
+
+  for (let scheme of schemes) {
+    schemeComponents.push(<li key={scheme.id}>{scheme.title}</li>);
+  }
+
+  return (
+    <div>
+      <label>Sort by:</label>
+      <select value={sort} onChange={(event) => updateSort(event.target.value)}>
+        <option value="id">ID</option>
+        <option value="name">Name</option>
+      </select>
+      <ul>(schemeComponents)</ul>
+    </div>
+  );
+
+  //???????? wtf is going on here????????????? why is it not working ahasdjhf ajksfhads f;aso fjas;
 }
